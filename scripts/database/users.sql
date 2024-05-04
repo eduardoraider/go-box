fbf8957ae79b5a9e98a9667eccfb67c9ce73c692
Eduardo Raider <eduraider@gmail.com>
2024-05-04 12:03:19 -0300

    users sql file updated

--- scripts/database/users.sql
+++ scripts/database/users.sql
@@ -5,7 +5,7 @@ CREATE TABLE users (
     password VARCHAR(200) NOT NULL,
     created_at TIMESTAMP DEFAULT current_timestamp,
     modified_at TIMESTAMP NOT NULL,
-    deleted BOLL NOT NULL DEFAULT false,
+    deleted BOOL NOT NULL DEFAULT false,
     last_login TIMESTAMP DEFAULT current_timestamp,
     PRIMARY KEY(id)
 )
