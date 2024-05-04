cf443e1cffef12b47f92ef11284e988ed8be9d8a
Eduardo Raider <eduraider@gmail.com>
2024-05-04 16:35:16 -0300

    feat(internal/queue): RabbitMQ methods implemented

--- internal/queue/rabbitmq.go
+++ internal/queue/rabbitmq.go
@@ -1,6 +1,7 @@
 package queue
 
 import (
+	"context"
 	amqp "github.com/rabbitmq/amqp091-go"
 	"time"
 )
@@ -17,9 +18,45 @@ type RabbitConnection struct {
 }
 
 func (rc *RabbitConnection) Publish(msg []byte) error {
+	c, err := rc.conn.Channel()
+	if err != nil {
+		return err
+	}
+	mp := amqp.Publishing{
+		DeliveryMode: amqp.Persistent,
+		Timestamp:    time.Now(),
+		ContentType:  "text/plain",
+		Body:         msg,
+	}
 
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+
+	return c.PublishWithContext(ctx,
+		"",
+		rc.cfg.TopicName,
+		false,
+		false,
+		mp)
 }
 
 func (rc *RabbitConnection) Consume() error {
-	
+	ch, err := rc.conn.Channel()
+	if err != nil {
+		return err
+	}
+
+	q, err := ch.QueueDeclare(rc.cfg.TopicName, false, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+
+	msgs, err := ch.Consume(q.Name, "", true, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+
+	for d := range msgs {
+		
+	}
 }
