f56a5bef7446b7f73da90fa33def59dd762d06ef
Eduardo Raider <eduraider@gmail.com>
2024-05-04 21:21:14 -0300

    feat(internal/users): user validate implemented

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/aws/aws-sdk-go v1.52.2
 	github.com/lib/pq v1.10.9
 	github.com/rabbitmq/amqp091-go v1.9.0
+	golang.org/x/crypto v0.22.0
 )
 
 require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- internal/users/entity.go
+++ internal/users/entity.go
@@ -1,15 +1,36 @@
 package users
 
-import "time"
+import (
+	"errors"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"time"
+)
+
+var (
+	ErrNameRequired     = errors.New("username is required")
+	ErrLoginRequired    = errors.New("username is required")
+	ErrPasswordRequired = errors.New("password is required and cannot be empty")
+	ErrPasswordLength   = errors.New("password must be at least 8 characters")
+)
 
 func New(name, login, password string) (*User, error) {
 	now := time.Now()
 	u := User{
 		Name:       name,
 		Login:      login,
-		Password:   password,
 		CreatedAt:  now,
 		ModifiedAt: now,
+	}
+
+	err := u.SetPassword(password)
+	if err != nil {
+		return nil, err
+	}
+
+	err = u.Validate()
+	if err != nil {
+		return nil, err
 	}
 
 	return &u, nil
@@ -24,4 +45,36 @@ type User struct {
 	ModifiedAt time.Time
 	Deleted    bool
 	LastLogin  time.Time
+}
+
+func (u *User) SetPassword(password string) error {
+	if password == "" {
+		return ErrPasswordRequired
+	}
+	if len(password) < 8 {
+		return ErrPasswordLength
+	}
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Println("error hashing password", err)
+	}
+	u.Password = string(hashedPassword)
+
+	return nil
+}
+
+func (u *User) Validate() error {
+	if u.Name == "" {
+		return ErrNameRequired
+	}
+	if u.Login == "" {
+		return ErrLoginRequired
+	}
+
+	blankPassword, _ := bcrypt.GenerateFromPassword([]byte(""), bcrypt.DefaultCost)
+	if u.Password == string(blankPassword) {
+		return ErrPasswordRequired
+	}
+
+	return nil
 }
