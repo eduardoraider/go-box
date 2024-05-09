a5ac0e21ddefce62537aebe1fc6ac30616b74c43
Eduardo Raider <eduraider@gmail.com>
2024-05-09 00:18:55 -0300

    refactor(internal/files): list files refactored

--- internal/files/list.go
+++ internal/files/list.go
@@ -4,29 +4,15 @@ import "database/sql"
 
 func List(db *sql.DB, folderId int64) ([]File, error) {
 	stmt := `SELECT * FROM files WHERE folder_id=$1 AND delete=false`
-	rows, err := db.Query(stmt, folderId)
-	if err != nil {
-		return nil, err
-	}
-
-	files := make([]File, 0)
-	for rows.Next() {
-		var f File
-
-		err := rows.Scan(&f.ID, &f.FolderId, &f.OwnerId, &f.Name, &f.Type,
-			&f.Path, &f.CreatedAt, &f.ModifiedAt, &f.Deleted)
-		if err != nil {
-			continue
-		}
-
-		files = append(files, f)
-	}
-
-	return files, nil
+	return selectAllFiles(db, stmt)
 }
 
 func ListRoot(db *sql.DB) ([]File, error) {
 	stmt := `SELECT * FROM files WHERE folder_id IS NULL AND delete=false`
+	return selectAllFiles(db, stmt)
+}
+
+func selectAllFiles(db *sql.DB, stmt string) ([]File, error) {
 	rows, err := db.Query(stmt)
 	if err != nil {
 		return nil, err
